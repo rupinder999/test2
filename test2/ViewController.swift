b5170eb6f2009134f098fda42f391d626910ad32
rupinder999 <chadharupinderpalsingh99@gmail.com>
2021-11-12 19:39:29 +0530

    Initial Commit

---
+++ test2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test2
+//
+//  Created by Rupinder Pal Singh on 12/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
